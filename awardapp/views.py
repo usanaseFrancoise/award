db63621c1cb18ffcc72cdca35380d15bd3a92729
usanase Francoise <fusanasencoise@gmail.com>
2019-10-25 11:57:22 +0200

    3 form

--- awardapp/views.py
+++ awardapp/views.py
@@ -119,8 +119,33 @@ def home(request):
     }
     return render(request,'post.html',context)
 
+@login_required(login_url='/accounts/login/')
+def profile(request,username):
+    profile = User.objects.get(username=username)
+    
+    try:
+        profile_details = Profile.get_by_id(profile.id)
+    except:
+        profile_details = Profile.filter_by_id(profile.id)
+    projects = Projects.get_profile_projects(profile.id)
+    
+    return render(request, 'users/profile.html',{"profile":profile,"profile_details":profile_details,"projects":projects}) 
 
 
+@login_required(login_url='/accounts/login/')
+def post_site(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProjectUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            home = form.save(commit=False)
+            home.profile =current_user
+            form.save()
+        return redirect('home')
+    else:
+        form =ProjectUploadForm()
+            
+    return render(request,'uploads.html',{"form":form,})
 
 
 
