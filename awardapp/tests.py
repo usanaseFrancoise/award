61db571409ea5f9998bf7c4b49c2cb28e62c2694
usanase Francoise <fusanasencoise@gmail.com>
2019-10-29 16:11:52 +0200

    test

--- awardapp/tests.py
+++ awardapp/tests.py
@@ -1,2 +1,20 @@
 from django.test import TestCase
-from .models import Profile,Project,Rate
+from .models import Project,User,Profile,Comments
+import datetime as dt
+
+
+class ProjectTestClass(TestCase):
+    '''
+    images test method
+    '''
+    def setUp(self):
+
+        self.user1 = User(username='fanny')
+        self.user1.save()
+          
+        self.image = Project(title = 'leaves',description = 'beautiful',user=self.user1,project_image = "image")
+        self.image.save_projects()
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Project))
